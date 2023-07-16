68d5e55c0c8b11ce2d21c6abdf66d057f8f32bba
Nilton <niltoneapontes@gmail.com>
2023-07-16 13:26:51 -0300

    feat: encapsulating variables

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -20,10 +21,14 @@ class GameViewModel: ViewModel() {
 
 
     // The current word
-    val word = MutableLiveData<String>()
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
 
     // The current score
-    val score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -64,17 +69,17 @@ class GameViewModel: ViewModel() {
         if (wordList.isEmpty()) {
             //gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
     fun onSkip() {
-        score.value = score.value!! - 1
+        _score.value = score.value!! - 1
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = score.value!! + 1
+        _score.value = score.value!! + 1
         nextWord()
     }
 }
